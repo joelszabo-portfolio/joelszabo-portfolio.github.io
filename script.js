355971fd4c45564625836d83f28915a021f3ad1b
joelszabo-portfolio <programmshowcase@gmail.com>
2025-03-21 22:16:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const textContainer = document.getElementById("text-container");
     
     // Position text container on the left 
-    textContainer.style.transform = "translate(-300px, -100px)";  // Moves it
+    textContainer.style.transform = "translate(-300px, -400px)";  // Moves it
 
 
     // Array containing the paths to the images we want to switch between
